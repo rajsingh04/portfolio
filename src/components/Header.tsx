87ecc6891418cab177a9f70324dab58d46f04ebe
Raj Singh <rajsingh18904@gmail.com>
2025-09-06 12:51:35 +0530

    changes in css for header and navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export function Header() {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2  h-[4.4rem] w-full
+        className="fixed top-0 left-1/2  !h-[4.4rem] w-full
     rounded-none  border-opacity-40 bg-linear-to-r from-cyan-500 to-blue-500 bg-opacity-80 shadow-lg
     shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25] sm:w-[36rem] sm:rounded-full"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
